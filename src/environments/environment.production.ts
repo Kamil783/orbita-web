36130b5e83e46dd65145bc3fd3eba476a20e001a
Kamil <galyamov.kamil5@gmail.com>
2026-03-30 17:46:22 +0300

    Update version

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   apiUrl: '',
-  appVersion: '0.1.0',
+  appVersion: '0.9.0',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   apiUrl: 'http://localhost:5000',
-  appVersion: '0.1.0',
+  appVersion: '0.9.0',
 };
